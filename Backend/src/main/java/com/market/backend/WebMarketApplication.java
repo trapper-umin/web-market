756b2328376da6e4a6532c1ed31cb69c90a6ab0a
Artem Krumin <trapper.programmer@gmail.com>
2022-12-27 10:37:47 +0300

    Initializing the repository

---
+++ Backend/src/main/java/com/market/backend/WebMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMarketApplication.class, args);
+	}
+
+}
